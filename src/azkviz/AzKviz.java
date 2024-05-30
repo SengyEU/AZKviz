dcd48b407272d4a6d9811d5a51874375e5cbdb94
Sengy <104589954+SengyEU@users.noreply.github.com>
2024-05-30 09:08:03 +0200

    Added player toggling + start of winning system #1

--- src/azkviz/AzKviz.java
+++ src/azkviz/AzKviz.java
@@ -4,7 +4,6 @@ import azkviz.gui.HraciPole;
 
 public class AzKviz {
 
-
     /**
      * 0 - empty (not used)
      * 1 - available
@@ -12,18 +11,38 @@ public class AzKviz {
      * 3 - player 2
      * 4 - yes/no
      */
+
+    private boolean player1 = true;
+
     private final int[][] buttonStates = new int[7][7];
 
     public void start(){
-        new HraciPole();
+        initializeButtonStates();
+        new HraciPole(this);
+    }
+
+    public boolean isPlayer1Playing(){
+        return player1;
+    }
+
+    public void setPlayer1Playing(boolean player1) {
+        this.player1 = player1;
+    }
+
+    public void play() {
+        //TODO: Implement winning system
     }
 
     public void initializeButtonStates(){
-        for(int i = 0; i < 7; i++){
-            for(int j = 0; j < 7; j++){
-                buttonStates[i][j] = 0;
+        for(int i = 1; i <= 7; i++){
+            for(int j = 0; j < i; j++){
+                buttonStates[i-1][j] = 1;
             }
         }
+    }
+
+    public int[][] getButtonStates() {
+        return buttonStates;
     }
 
     public void setButtonState(int state, int row, int col){
@@ -33,5 +52,6 @@ public class AzKviz {
     public int getButtonState(int row, int col){
         return buttonStates[row][col];
     }
+
 
 }
